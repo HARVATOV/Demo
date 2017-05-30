60e5e526ae97c6b6fa1a40448719fbd13249eb4f
HARVATOV <p.harvtov@abv.bg>
2017-05-30 18:27:32 +0300

    Add first file

---
+++ Lection 3 - Data&Variables/6 String and Objects.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Practicing_Floating_Point_Num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName= Console.ReadLine();
+            byte age = byte.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+            long personalId = long.Parse(Console.ReadLine());
+            int employeeNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"First name: {firstName}");
+            Console.WriteLine($"Last namer: {lastName}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Gender: {gender}");
+            Console.WriteLine($"Personal ID: {personalId}");
+            Console.WriteLine($"Unique Employee number: {employeeNumber}");
+
+
+        }
+    }
+}
